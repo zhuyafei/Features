477ff5c6a7eae160ae19e8fe5bf092729b227a5d
zhuyafei <zhuyafei4520@163.com>
2015-01-30 22:31:29 +0800

    Update some contents

--- RGBhistogram.m
+++ RGBhistogram.m
@@ -4,7 +4,6 @@ RGB_bins = [16 16 16];
 R = image_rgb(:,:,1);
 G = image_rgb(:,:,2);
 B = image_rgb(:,:,3);
-
 rr = min( floor(R*RGB_bins(1)) + 1, RGB_bins(1) );
 gg = min( floor(G*RGB_bins(2)) + 1, RGB_bins(2) );
 bb = min( floor(B*RGB_bins(3)) + 1, RGB_bins(3) );
