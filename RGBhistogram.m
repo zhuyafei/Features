3df3b8d46fc2da46900f345098288d11175c0eba
zhuyafei <zhuyafei4520@163.com>
2015-01-30 10:53:18 +0800

    Initial commit

---
+++ RGBhistogram.m
@@ -0,0 +1,13 @@
+image = imread('3.jpg');
+image_rgb = im2double( image );
+RGB_bins = [16 16 16];
+R = image_rgb(:,:,1);
+G = image_rgb(:,:,2);
+B = image_rgb(:,:,3);
+
+rr = min( floor(R*RGB_bins(1)) + 1, RGB_bins(1) );
+gg = min( floor(G*RGB_bins(2)) + 1, RGB_bins(2) );
+bb = min( floor(B*RGB_bins(3)) + 1, RGB_bins(3) );
+Q_rgb = (rr-1) * RGB_bins(2) * RGB_bins(3) + ...
+    (gg-1) * RGB_bins(3) + ...
+    bb + 1;
